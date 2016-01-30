f18f224363334fec408363c20f6dee7ce086d2da
Daniel Prundianu <d.prundianu@gmail.com>
2016-01-30 02:29:47 +0100

    initial commit

---
+++ iThermoPi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iThermoPi
+//
+//  Created by Daniel Prundianu, BSc. on 29/01/16.
+//  Copyright © 2016 Daniel Prundianu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
